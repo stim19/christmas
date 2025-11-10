2adae9da7f9ea4c80083d3f2bedc4e0d6cb78e65
stim19 <yoshiyomimiyori@gmail.com>
2025-11-10 13:09:55 -0500

    Configured GTest and set up CTest

--- tests/test_db.cpp
+++ tests/test_db.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
-#include <string>
+#include <gtest/gtest.h>
 #include "../db.h"
 
+TEST(DBConstructor, Logs) {
+    DBEngine db("test.db", true);
+    // If constructor crashes, test fails automatically
+    SUCCEED();
+}
+
+TEST(DBLoad, Sample) {
+    DBEngine db("test.db");
+    // TODO: ADD load test here
+    EXPECT_TRUE(true);
+}
+
 int main(int argc, char** argv) {
-    try {
-        std::cout << "=== Constructor test ===\n";
-    }
-    catch (const std::exception& e) {
-        std::cerr << "Test failed: " << e.what() << "\n";
-    }
-    std::cout << "All tests passed\n";
-    return 0;
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
